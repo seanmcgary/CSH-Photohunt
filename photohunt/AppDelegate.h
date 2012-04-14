05574cff3b7c55bd1eee628d21a5fbc7e8d1dd85
Sean McGary <sean.mcgary@gmail.com>
2012-04-14 15:55:03 -0400

    Initial Commit

---
+++ photohunt/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  photohunt
+//
+//  Created by Sean McGary on 4/14/12.
+//  Copyright (c) 2012 RIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ photohunt/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  photohunt
+//
+//  Created by Sean McGary on 4/14/12.
+//  Copyright (c) 2012 RIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
